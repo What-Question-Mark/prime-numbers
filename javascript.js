2f1ac2aa1de4911fb4c5b84669a816d6d5baa26a
what? <jack.weller@hotmail.com>
2022-09-01 19:30:55 +1000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
+const number = 1 
+let flag = False
 
+for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+        flag = false;
+        break;
+    }
+}
+
+if (flag) {
+    console.log(`${number} is a prime number`);
+} else {
+    console.log(`${number} is a not prime number`);
+}
